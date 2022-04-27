26b8346b416a4beafda4f381a13b9b684e881c4a
Michael J <johnson_michael1329@yahoo.com>
2022-04-27 15:13:05 -0500

    Create Components folder + Add Components DisplayMusic and NavBar to Components folder.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <div >
-        <nav>
-          Music Library App
-        </nav>
-      </div>
-      <div>
-        <strong> The Table will be here</strong>
-        <p>Contents will go below</p>
-      </div>
+    <div>
+      music LIbrary App
     </div>
   );
 }
