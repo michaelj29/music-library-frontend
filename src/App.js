f718f3b7d8bf8b6b3d5670278b4f8c86216bc61b
Michael J <johnson_michael1329@yahoo.com>
2022-04-27 15:27:20 -0500

    Create NavBar with react-bootstrap.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import NavBar from './Components/NavBar/NavBar';
 
 import './App.css';
 
@@ -9,7 +10,12 @@ import './App.css';
 function App() {
   return (
     <div>
-      music LIbrary App
+      <div>
+        <NavBar sticky='top'/>
+      </div>
+      <div>
+        table will go here
+      </div>
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Button, Form, FormControl } from 'react-bootstrap';
+
+const NavBar = (props) => {
+
+
+    return ( 
+        <div>
+           <Navbar bg="light" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#">Music Library</Navbar.Brand>
+
+                    <Form className="d-flex">
+                        <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Container>
+            </Navbar>
+        </div>
+     );
+}
+ 
+export default NavBar;
