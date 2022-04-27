b7dcf0a4b7eef7013c00d418ffc905eb4298b222
Michael J <johnson_michael1329@yahoo.com>
2022-04-27 16:10:45 -0500

    Create table for DisplayMusic.jsx file + style table with react bootstrap.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,36 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import NavBar from './Components/NavBar/NavBar';
-
 import './App.css';
+import DisplayMusic from './Components/DisplayMusic/DisplayMusic';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(()=>{
+    getAllSongs();
+  }, [])
+
+  async function getAllSongs(){
+    try {
+      let response = await axios.get('http://127.0.0.1:8000/music/');
+      setSongs(response.data)
+    } catch (err) {
+      console.log("Error in getAllSongs function in DisplayMusic.jsx file")
+    }
+  }
+
   return (
     <div>
       <div>
         <NavBar sticky='top'/>
       </div>
       <div>
-        table will go here
+        <DisplayMusic songs={songs} />
       </div>
     </div>
   );
--- src/Components/DisplayMusic/DisplayMusic.jsx
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -1,1 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table'
+
+
+
+const DisplayMusic = (props) => {
+
+
+
+    return ( 
+        <div>
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.songs.map((song, index)=> {
+                        return (
+                            <tr key={index + 1}>
+                                <th>{song.id}</th>
+                                <th>{song.title}</th>
+                                <th>{song.artist}</th>
+                                <th>{song.album}</th>
+                                <th>{song.release_date}</th>
+                                <th>{song.genre}</th>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+    </Table>
+        </div>
+     );
+}
+ 
+export default DisplayMusic;
